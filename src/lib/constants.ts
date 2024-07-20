5f778cc128a12716b6959dc933d71c201e880360
Braijan <brian@briancharlessmith.com>
2024-07-20 17:59:33 -0400

    init commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,33 @@
+export const POSTS: { title: string; href: string; description: string }[] = [
+  {
+    title: 'React',
+    href: '/blog/react',
+    description: 'Learn React.js and Next.js in a simple to understand article',
+  },
+  {
+    title: 'Javascript',
+    href: '/blog/javascript',
+    description: "Learn what's new in the javascript world",
+  },
+  {
+    title: 'CSS',
+    href: '/blog/css',
+    description: 'Everything about CSS and the new features',
+  },
+  {
+    title: 'Performance',
+    href: '/blog/performance',
+    description: 'How to make your next app Blazing fast',
+  },
+  {
+    title: 'Animation',
+    href: '/blog/animations',
+    description: 'Everything you need to know about animations.',
+  },
+  {
+    title: 'Career',
+    href: '/blog/career',
+    description:
+      'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
+  },
+];
---
+++ src/lib/placeholder-data.ts
@@ -0,0 +1,26 @@
+export const categories = [
+  'react',
+  'javascript',
+  'css',
+  'animation',
+  'performance',
+  'database',
+];
+
+export const popularPosts = [
+  {
+    title: 'React 19 RC',
+  },
+  {
+    title: 'Learn React in 2024',
+  },
+  {
+    title: 'Embracing Vim: The Unsung Hero of Code Editors',
+  },
+  {
+    title: 'Spaces vs. Tabs: The Indentation Debate Continues',
+  },
+  {
+    title: 'The Power of Static Typing in Programming',
+  },
+];
