1570c05444491aa913da373b63b74254a852dcca
Braijan <brian@briancharlessmith.com>
2024-07-20 19:46:03 -0400

    server action and newsletter widget done

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,16 @@
+'use client';
+
 import { POSTS } from '@/lib/constants';
 import { Icons } from './icons';
 import Link from 'next/link';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
+import { createSubscriber } from '@/lib/actions';
+import { useFormState } from 'react-dom';
 
 export default function Footer() {
+  const initialState = { message: '', errors: {} };
+  const [state, dispatch] = useFormState(createSubscriber, initialState);
   return (
     <footer className="bg-gray-100 py-8 dark:bg-gray-800 mt-10">
       <div className="container mx-auto px-4 md:px-6">
@@ -86,20 +92,40 @@ export default function Footer() {
               Subscribe to my newsletter to stay up-to-date with the latest news
               and updates.
             </p>
-            <form action="">
+            <form action={dispatch}>
               <div className="flex space-x-2">
                 <Input
                   type="email"
+                  name="email"
+                  id="email"
                   placeholder="Enter your email"
                   className="flex-1"
+                  defaultValue=""
+                  aria-describedby="email-error"
                 />
                 <Button>Subscribe</Button>
+              </div>
+              <div
+                id="email-error"
+                aria-label="polite"
+                aria-atomic="true"
+                className="px-1"
+              >
+                {state?.errors?.email &&
+                  state.errors.email.map((error: string) => (
+                    <p key={error} className="text-xs text-red-500">
+                      {error}
+                    </p>
+                  ))}
+                {!state?.errors?.email && (
+                  <p className="text-xs text-green-500">{state?.message}</p>
+                )}
               </div>
             </form>
           </div>
         </div>
-        <div className="mt-8 border-t border-gray-200 pt-4 text-center text-xs text-gray-500 dark:text-gray-400 dark:border-gray-700">
-          &copy; 2024 K3sler Sec. All rights reserved.
+        <div className="mt-8 border-t border-gray-200 pt-4 text-center text-xs text-gray-500 dark:border-gray-700 dark:text-gray-400">
+          &copy; 2024 Coding Jitsu. All rights reserved.
         </div>
       </div>
     </footer>
