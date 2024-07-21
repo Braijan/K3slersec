4516f247740d223b652fc050ed17394601ee72da
Braijan <brian@briancharlessmith.com>
2024-07-21 17:09:41 -0400

    update baseUrl

--- src/config/site.ts
+++ src/config/site.ts
@@ -13,8 +13,8 @@ export const siteConfig: SiteConfig = {
   name: 'K3strel Sec',
   description:
     'An Open source Technical Blog related to cybersecurity, pentesting, and bug bounty hunting.',
-  url: 'https://k3strelsec.vercel.app/',
-  ogImage: 'https://k3strelsec.vercel.app/og',
+  url: 'https://k3strelsec.com/',
+  ogImage: 'https://k3strelsec.com/og',
   links: {
     twitter: 'https://x.com/x_k3strel_x',
     github: 'https://github.com/Braijan',
