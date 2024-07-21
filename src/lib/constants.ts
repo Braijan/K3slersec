65f71f4cb4e9f2cbd23b60de46ffe4ad35c7b9f3
Braijan <brian@briancharlessmith.com>
2024-07-20 22:36:46 -0400

    updated blogs

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,33 +1,50 @@
 export const POSTS: { title: string; href: string; description: string }[] = [
   {
-    title: 'React',
-    href: '/blog/react',
-    description: 'Learn React.js and Next.js in a simple to understand article',
-  },
-  {
-    title: 'Javascript',
-    href: '/blog/javascript',
-    description: "Learn what's new in the javascript world",
-  },
-  {
-    title: 'CSS',
-    href: '/blog/css',
-    description: 'Everything about CSS and the new features',
-  },
-  {
-    title: 'Performance',
-    href: '/blog/performance',
-    description: 'How to make your next app Blazing fast',
-  },
-  {
-    title: 'Animation',
-    href: '/blog/animations',
-    description: 'Everything you need to know about animations.',
-  },
-  {
-    title: 'Career',
-    href: '/blog/career',
+    title: 'Penetesting',
+    href: '/blog/penetration-testing',
     description:
-      'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
+      'Insights and techniques for identifying and mitigating security vulnerabilities through simulated cyber attacks.',
+  },
+  {
+    title: 'Bug Bounty',
+    href: '/blog/bug-bounty',
+    description:
+      'Strategies and tips for participating in bug bounty programs, including successful reports and platform reviews.',
+  },
+  {
+    title: 'OSINT',
+    href: '/blog/osint',
+    description:
+      'Tools, techniques, and resources for gathering and analyzing publicly available information.',
+  },
+  {
+    title: 'Ethical Hacking',
+    href: '/blog/ethical-hacking',
+    description:
+      'Guides on ethical hacking practices, certifications, legal considerations, and real-world scenarios.',
+  },
+  {
+    title: 'News/Updates',
+    href: '/blog/cybersecurity-news-and-updates',
+    description:
+      'Latest industry news, vulnerability disclosures, and insights into recent security breaches.',
+  },
+  {
+    title: 'Infosec',
+    href: '/blog/infosec',
+    description:
+      'Best practices, risk management strategies, and compliance policies to enhance information security.',
+  },
+  {
+    title: 'Tools/Resources',
+    href: '/blog/tools-and-resources',
+    description:
+      'Reviews, comparisons, and guides on essential cybersecurity tools and open-source projects.',
+  },
+  {
+    title: 'CTF',
+    href: '/blog/capture-the-flag',
+    description:
+      'Announcements, write-ups, and solutions for Capture The Flag events, including training resources and preparation tips.',
   },
 ];
